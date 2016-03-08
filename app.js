73ff8d6df5400cb0f9510f826c9f113a18c0ade3
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-03-08 19:01:49 +0000

    Add Mongoose. Add API

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
+var db = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
