5d0f769f23e6675edfa99b855ff9bdf1949e0abe
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-03-12 12:50:58 +0000

    Update DB Schema

--- db/models.js
+++ db/models.js
@@ -1,19 +1,42 @@
 var db = require('mongoose');
 var Schema = db.Schema;
+var bcrypt = require('bcrypt');
 
-db.model('Events', new Schema({
-  name: String,
-  place: String,
-  date: Date,
-  description: String,
-  organiser: {
-    type: Schema.Types.ObjectId, 
-    ref: 'User'
-  } 
-}));
+var eventSchema =  new Schema({
+                     name: String,
+                     place: String,
+                     date: Date,
+                     description: String,
+                     organiser: {
+                       type: Schema.Types.ObjectId, 
+                       ref: 'User'
+                     } 
+                   });
+db.model('Events', eventSchema);
 
-db.model('User', new Schema({
-  shortcode: String,
-  name: String,
-  password: String
-}));
+var userSchema =  new Schema({
+                   shortcode: String,
+                   name: String,
+                   password: String
+                 });
+var User = db.model('User', userSchema);
+
+userSchema.methods.validPassword = function(passwd) {
+  User.findOne({shortcode: this.shortcode})
+      .select('password')
+      .exec(function(err, user) {
+        if(err) {
+          console.log(err);
+          return false;
+        }
+        bcrypt.compare(passwd, user.password, function(err, res){
+          if(err) {
+            console.log(err);
+            return false;
+          } else {
+            return res == true;
+          }
+        })
+      })
+};
+
